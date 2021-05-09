130bf7c13b0e252185c18d81ec3a7dcb9a9b6ca9
bettaibi <bettaibinidhal00@gmail.com>
2021-05-10 01:09:05 +0200

    next js app

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <div>
+            <Head>
+                <title>About page </title>
+            </Head>
+
+            this is an about page
+        </div>
+    )
+}
+
+export default About
